6ac67eecbd4c00f0b523b4cea0f7da770e7a9b2a
Boris Staletic <boris.staletic@protonmail.com>
2021-11-18 00:11:10 +0100

    CMake is clinically insane and forces us to fight it
    Corss-compiling is hard and cmake only makes it harder.
    None of the sane ways of setting a CMake variable work, because cmake
    will try to, idiotically, check whether it can compile and run things
    with the cross-compiler. If that doesn't sound wrong, read it again.
    Also, the fucker leaks CFLAGS into ASM flags. Now we use that awfully
    horrible `set($ENV{CFLAGS})` thing. That works around the cmake's
    drinking issues. If you think that's weird, well... you've just entered
    my favourite drinking game - drink every time cmake frustrates you.

--- toolchain_files/arm-linux-gnueabihf.cmake
+++ toolchain_files/arm-linux-gnueabihf.cmake
@@ -1,8 +1,8 @@
-set(CMAKE_C_FLAGS_INIT "-mcpu=generic-armv7-a")
-set(CMAKE_CXX_FLAGS_INIT "-mcpu=generic-armv7-a")
-
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set($ENV{CFLAGS} "-mcpu=generic-armv7-a")
+set($ENV{CXXFLAGS} "-mcpu=generic-armv7-a")
+
+set(CMAKE_C_FLAGS "-mcpu=generic-armv7-a")
+set(CMAKE_CXX_FLAGS "-mcpu=generic-armv7-a")
